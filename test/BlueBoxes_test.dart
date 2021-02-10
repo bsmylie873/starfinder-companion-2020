7e75075ed522ff7bbf47aea390ef2eff2a1a0a1b
40184143 <cclarke44@qub.ac.uk>
2021-02-10 13:54:18 +0000

    Backup commit

--- test/BlueBoxes_test.dart
+++ test/BlueBoxes_test.dart
@@ -8,20 +8,65 @@ import 'package:testflutter/BlueBoxes.dart';
 void main() {
   group('BlueBoxes', () {
   testWidgets('Blue Box text should read as specified value', (WidgetTester tester) async {
-    await tester.pumpWidget(BlueBox());
-    //final titleFinder = find.text('Character Generation');
-    //expect(titleFinder, findsOneWidget);
+    await tester.pumpWidget(MaterialApp(
 
-    
-  });
-  test('Blue Box1 text should read as specified value', () {
+    home: BlueBox()));
+    final titleFinder = find.text('Character Generation');
+    expect(titleFinder, findsOneWidget);
 
   });
-  test('Blue Box2 text should read as specified value', () {
+  testWidgets('Blue Box1 text should read as specified value', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(
+
+          home: BlueBox1()));
+      final titleFinder = find.text('Character Races');
+      expect(titleFinder, findsOneWidget);
 
   });
-  test('Blue Box3 text should read as specified value', () {
+  testWidgets('Blue Box2 text should read as specified value', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+
+        home: BlueBox2()));
+    final titleFinder = find.text('Skill Index');
+    expect(titleFinder, findsOneWidget);
 
   });
+  testWidgets('Blue Box3 text should read as specified value', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+
+        home: BlueBox3()));
+    final titleFinder = find.text('Class Index');
+    expect(titleFinder, findsOneWidget);
+  });
+  testWidgets('Blue Box4 text should read as specified value', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+
+        home: BlueBox4()));
+    final titleFinder = find.text('Magic Index');
+    expect(titleFinder, findsOneWidget);
+  });
+  testWidgets('Blue Box5 text should read as specified value', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+
+        home: BlueBox5()));
+    final titleFinder = find.text('GM Rules');
+    expect(titleFinder, findsOneWidget);
+  });
+  testWidgets('Blue Box6 text should read as specified value', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+
+        home: BlueBox6()));
+    final titleFinder = find.text('Character Sheet');
+    expect(titleFinder, findsOneWidget);
+  });
+  testWidgets('Blue Box7 text should read as specified value', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+
+        home: BlueBox7()));
+    final titleFinder = find.text('Saved Char Sheets');
+    expect(titleFinder, findsOneWidget);
+  });
+
+
   });
 }
