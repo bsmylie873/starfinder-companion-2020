d9c9f563ba563f3a8a283ecf6c443c2e7bbd2980
Brandon James Smylie <bsmylie02@qub.ac.uk>
2021-03-17 13:30:06 +0000

    Small license change

--- lib/screens/openGameContentScreen.dart
+++ lib/screens/openGameContentScreen.dart
@@ -6,7 +6,7 @@ import 'package:flutter/rendering.dart';
 //Open Game Content screen.
 class OpenGameContentScreen extends StatelessWidget {
   String text =
-      "Except for material designated as Product Identity (see above), the game mechanics of this Starfinder Companion 2020 app are Open Game Content, as defined in the Open Game License version 1.0a Section 1(d). No portion of this work other than the material designated as Open Game Content may be reproduced in any form without written permission.";
+      "Except for material designated as Product Identity, the game mechanics of this Starfinder Companion 2020 app are Open Game Content, as defined in the Open Game License version 1.0a Section 1(d). No portion of this work other than the material designated as Open Game Content may be reproduced in any form without written permission.";
 
   @override
   Widget build(BuildContext context) {
